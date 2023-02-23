e62bf016be97643a1163688595b532878266dbad
Johnny Ansah <jon.ansah.15@gmail.com>
2023-02-23 09:51:14 -0800

    another comit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * _isupper - checks for uppercase character
 * @c: Variable text
@@ -6,13 +7,12 @@
 */
 int _isupper(int c)
 {
- if (c >= 'A' && c <= 'Z')
- {
- return (1);
- }
- else
+	if (c >= 'A' && c <= 'Z')
 {
- return (0);
+	return (1);
 }
-
+	else
+{
+	return (0);
+}
 }
