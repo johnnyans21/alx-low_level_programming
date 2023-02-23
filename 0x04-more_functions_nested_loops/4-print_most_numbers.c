8223be947d5840a0e9bce193c81231290e4fb5d9
Johnny Ansah <jon.ansah.15@gmail.com>
2023-02-23 15:23:14 -0800

    another updated for printing most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,18 +7,17 @@
  */
 void print_most_numbers(void)
 {
- int c;
-
- for (c = 48; c < 58; c++)
- {
- if (c != 50)
- {
- if (c != 52)
- {
- _putchar(c);
- }
- }
- }
- _putchar('\n');
+int c;
+for (c = 48; c < 58; c++)
+{
+if (c != 50)
+{
+if (c != 52)
+{
+_putchar(c);
+}
+}
+}
+_putchar('\n');
 }
 
